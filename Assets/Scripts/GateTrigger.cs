3aa63adee65b614a1004c22cbf615cc68e6e39a2
Robert Li <robertgiali1996@gmail.com>
2019-11-02 23:43:47 -0400

    initial commit

---
+++ Assets/Scripts/GateTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateTrigger : MonoBehaviour
+{
+    public GameObject targetObject;
+    private bool isTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerUnit" && isTriggered == false)
+        {
+            isTriggered = true;
+            Debug.Log("door triggered");
+            removeObstacle();
+        }
+    }
+
+    private void removeObstacle()
+    {
+        targetObject.transform.Translate(0, 5, 0);
+    }
+}
